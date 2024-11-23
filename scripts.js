b14fd89b851e74e9b105e4ae743ee7159780cdbc
imhoward <howardian58@gmail.com>
2024-11-23 11:57:18 -0500

    Add button that resets board and changes sq size
    I added a button that when clicked, removes all the children of the main
    container holding the divs, and then shows a prompt that asks for the
    size of each side of the new grid. This size will be the size of the
    squares in the grid, not the size of the grid itself. This prompt is
    inside a loop in order to reprompt in case the user enters a number
    higher than 100 or lower than 1.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,48 @@
 
 
 let mainContainer = document.querySelector(".main-container");
-/*
-let div = document.createElement("div");
-mainContainer.append(div);
-*/
+let body = document.querySelector("body");
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.classList.add("divs");
-    div.addEventListener('mouseover', function(e) {
-        div.classList.add('colored-divs');
-    })
-    mainContainer.append(div);
+let btn = document.createElement("button");
+btn.classList.add("btn");
+btn.innerText = "Reset Board";
+body.append(btn);
+
+function createDivGrid(size) {
+    for (let i = 0; i < (size*size); i++) {
+        let div = document.createElement("div");
+        div.classList.add("divs");
+
+        let divSize = 900 / size;
+        div.style.width = String(divSize) + "px";
+        div.style.height = String(divSize) + "px";
+    
+        div.addEventListener('mouseover', function(e) {
+            div.classList.add('colored-divs');
+        });
+    
+        mainContainer.append(div);
+    }
 }
+
+let divs = document.querySelector(".divs");
+
+createDivGrid(16);
+
+btn.addEventListener('click', function(e) {
+    while (mainContainer.hasChildNodes()) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+
+    let loopTrue = false;
+
+    while(loopTrue == false) { 
+        let size = prompt("How many squares do you want per side?");
+
+        if (size <= 100 && size > 0) {
+            loopTrue = true;
+            createDivGrid(size);
+        } 
+    } 
+});
 
